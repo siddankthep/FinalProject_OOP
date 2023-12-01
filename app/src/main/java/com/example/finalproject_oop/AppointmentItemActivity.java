3b6f472f412b362d6181975156fed68e79c9dd64
siddankthep <quan.nguyen.210257@student.fulbright.edu.vn>
2023-12-01 11:25:19 +0700

    Add Appointment, update login register

---
+++ app/src/main/java/com/example/finalproject_oop/AppointmentItemActivity.java
@@ -0,0 +1,14 @@
+package com.example.finalproject_oop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppointmentItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appointment_item);
+    }
+}
