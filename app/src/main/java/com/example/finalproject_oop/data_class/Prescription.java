935d60ed5d06e599a219d50e15c367882af18ab9
siddankthep <quan.nguyen.210257@student.fulbright.edu.vn>
2023-12-09 12:39:40 +0700

    add prescription class and DAO

---
+++ app/src/main/java/com/example/finalproject_oop/data_class/Prescription.java
@@ -0,0 +1,60 @@
+package com.example.finalproject_oop.data_class;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity{tableName = "prescription"}
+public class Prescription {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    private String username; // Patient's username
+    private String medicationName;
+    private int quantity;
+    private String instructions;
+
+    // Constructors, getters, and setters
+
+    // For simplicity, you can generate these methods in Android Studio
+
+    public Prescription(String username, String medicationName, int quantity, String instructions) {
+        this.username = username;
+        this.medicationName = medicationName;
+        this.quantity = quantity;
+        this.instructions = instructions;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMedicationName() {
+        return medicationName;
+    }
+
+    public void setMedicationName(String medicationName) {
+        this.medicationName = medicationName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+}
+
